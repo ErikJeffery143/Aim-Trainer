083a0321d2a0c856ce5f8cc487b26d5f9c7565e4
Erik Jeffery <JeffErik143@GoTVDSB.ca>
2021-12-17 14:39:46 -0500

    Add project files.

---
+++ Game1.cs
@@ -0,0 +1,104 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System.Collections.Generic;
+
+namespace Aim_Trainer
+{
+     
+
+
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+
+        List<Rectangle> Targets;
+        Texture2D Target;
+        Texture2D Bullethole;
+        List<Rectangle> Bullets;
+        SoundEffect Gunshotsoundeffect;
+        Texture2D crosshair;
+
+        MouseState mouseState;
+        KeyboardState keyboardState;
+         
+        enum Screen
+        {
+            intro,
+            shooter,
+            good,
+            fail
+        }
+
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+
+            _graphics.PreferredBackBufferWidth = 1600;  // set this value to the desired width of your window
+            _graphics.PreferredBackBufferHeight = 900;   // set this value to the desired height of your window
+            _graphics.ApplyChanges();
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+
+
+
+
+
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            Bullethole = Content.Load<Texture2D>("bullet_hole");
+            crosshair = Content.Load<Texture2D>("crosshair");
+            Target = Content.Load<Texture2D>("target_color");
+            Gunshotsoundeffect = Content.Load<SoundEffect>("Gunshot+shell");
+
+
+
+            // TODO: use this.Content to load your game content here
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            keyboardState = Keyboard.GetState();
+            mouseState = Mouse.GetState();
+
+
+
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+            _spriteBatch.Begin();
+
+
+
+
+
+
+            // TODO: Add your drawing code here
+            _spriteBatch.End();
+            base.Draw(gameTime);
+        }
+    }
+}
